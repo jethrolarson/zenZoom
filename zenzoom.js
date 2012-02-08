a4d7eef7633b00b1d3aca3167ec53afde38ea3a2
Jethro Larson <jethrolarson@gmail.com>
2012-02-07 21:23:34 -0800

    Plugin is now disabled on small windows. Images that are wider than the window should no longer get distorted.

--- zenzoom.js
+++ zenzoom.js
@@ -2,13 +2,13 @@
     #copyright 2010 Jethro Larson http://jethrolarson.com
     #license MIT or GPL Version 2 licenses. Like jQuery http://jquery.org/license
 */
-$(function(){
-  $.fn.zenZoom = function(){
+$(function() {
+  $.fn.zenZoom = function() {
     var $zoom = $('<div id="zoom"></div>').appendTo(document.body),
         $self = this;
-    $self.bind("click.zoomIn", function(){
+    $self.live("click.zoomIn", function() {
       var $that = $(this);
-      if($zoom.is(":visible")){
+      if ($zoom.is(":visible")) {
         zoomOut();
         return false;
       }
@@ -22,62 +22,66 @@ $(function(){
         .height($img.height())
         .width($img.width());
       $that.addClass("loading");
-      $("<img src='" + this.href + "' style='display:none' />").load(function(){
+      $("<img src='" + this.href + "' style='display:none' />").load(function() {
         $zoom.show();
         var $t = $(this),
             ww = $(window).width() - 24,
             wh = $(window).height() - 38,
-            w=$t.width(),h=$t.height();
-        if(h>wh){
+            w = $t.width(), h = $t.height();
+        if(w > ww){
+          w = ww;
+          $t.width(w);
+        }else if(h > wh){
           h = wh;
           $t.height(wh);
         }
-        if(w>ww){
-          w = ww;
-          $t.width(w);
-        }
+        
         w = $t.width();
         h = $t.height();
-        $zoom.css("opacity",.4).animate({
+        $zoom.css("opacity", 0.4).animate({
           width: w,
           height: h,
           left: $(window).scrollLeft() + (ww / 2) - (w / 2),
           top: $(window).scrollTop() + (wh / 2) - (h / 2) +12
         },
         300,
-        function(){
+        function() {
           $t.fadeIn(300);
-          $zoom.css("opacity",1)
+          $zoom.css("opacity", 1);
         });
         $that.removeClass("loading");
         $("body").addClass('zoomOn');
-      }).bind("error", function(){
+      }).bind("error", function() {
         $that.removeClass("loading").removeClass("zoom").addClass("noZoom").unbind("click.zoomIn");
       }).appendTo($zoom.empty());
       return false;
     });
-    function zoomOut(){
+    function zoomOut() {
       var $img = $zoom.data("img");
       $("body").removeClass('zoomOn');
-      $zoom.css("opacity",.4).children("img").hide().end().animate({
+      $zoom.css("opacity", 0.4).children("img").hide().end().animate({
         height: $img.height(),
         width: $img.width(),
         left: $img.offset().left,
         top: $img.offset().top
-      }, 300, function(){
+      }, 300, function() {
         $zoom.hide();
       });
     }
-    $("body").click(function(){
-      if($zoom.is(":visible")){
+    $("body").click(function() {
+      if ($zoom.is(":visible")) {
         zoomOut();
         return false;
       }
-    }).keypress(function(e){
-      if($zoom.is(":visible") && e.keyCode == 27){
+    }).keypress(function(e) {
+      if ($zoom.is(":visible") && e.keyCode == 27) {
         zoomOut();
         return false;
       }
     });
   };
+  //Disabling zen zoom if window is small, as the OS can do a better job
+  if($(window).width() > 500){
+    $(".zoom").zenZoom();
+  }
 });
